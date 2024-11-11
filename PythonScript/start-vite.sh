105ad0f0341fa1a81ec1169eeede53aafe3dfac7
udhaya1148 <59645398+udhaya1148@users.noreply.github.com>
2024-11-11 11:51:43 +0530

    Create start-vite.sh

---
+++ PythonScript/start-vite.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Navigate to your project directory
+cd /home/netcon/netplan/Network-configuration/PythonScript
+
+# Build the project
+npm run build
+
+# Start the server using `serve` on port 3000 or any preferred port
+#serve -s dist -l 3003
+
+npm run preview
