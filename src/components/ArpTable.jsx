d6ebad4ba29661001622b6467af97f240de85e0d
udhaya1148 <59645398+udhaya1148@users.noreply.github.com>
2024-11-19 13:00:31 +0530

    Update ArpTable.jsx

--- src/components/ArpTable.jsx
+++ src/components/ArpTable.jsx
@@ -8,7 +8,7 @@ const ArpTable = () => {
   // Function to fetch ARP data
   const fetchArpData = async () => {
     try {
-      const response = await fetch("http://172.18.1.224:8000/api/arp");
+      const response = await fetch("localhost:8000/api2/arp");
       if (!response.ok) {
         throw new Error(`Failed to fetch ARP data: ${response.statusText}`);
       }
