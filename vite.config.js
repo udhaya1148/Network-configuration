412a13e7456c0c479cad32e7b66d8e4303fe20e2
udhaya1148 <59645398+udhaya1148@users.noreply.github.com>
2024-11-19 14:25:55 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,19 +7,19 @@ export default defineConfig({
     host: true,
     proxy: {
       '/api1': {
-        target: 'http://localhost:5000', // Flask server URL
+        target: 'http://localhost:5001', // Flask server URL
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api1/, ''), // Correct path replacement for /api1
       },
       '/api2': {
-        target: 'http://localhost:5001', // Flask server URL
+        target: 'http://localhost:5002', // Flask server URL
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api2/, ''), // Correct path replacement for /api2
       },
     },
   },
   preview: {
-    port: 5004,
+    port: 5000,
     host: true,
   },
 });
