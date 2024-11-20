1a9890f0a69ba9987cbe07691bf13769e3439f37
udhaya1148 <59645398+udhaya1148@users.noreply.github.com>
2024-11-20 11:50:47 +0530

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -24,4 +24,21 @@ sudo chmod +x /bin/arp-pythonscript.py
 sudo chmod +x /home/netcon/Network-configuration/PythonScript/start-vite.sh
 
 
+# Network Configuration
+sudo cp -i /home/netcon/Network-configuration/PythonScript/Network-configuration.py /bin
+sudo chmod +x /bin/Network-configuration.py
+(crontab -l 2>/dev/null; echo "@reboot /usr/bin/python3 /bin/Network-configuration.py") | sudo crontab -
+
+# Static ARP
+sudo cp -i /home/netcon/Network-configuration/PythonScript/arp-pythonscript.py /bin
+sudo chmod +x /bin/arp-pythonscript.py
+(crontab -l 2>/dev/null; echo "@reboot /usr/bin/python3 /bin/arp-pythonscript.py") | sudo crontab -
+
+# React Application
+sudo chmod +x /home/netcon/Network-configuration/PythonScript/start-vite.sh
+(crontab -l 2>/dev/null; echo "@reboot /home/netcon/Network-configuration/PythonScript/start-vite.sh") | sudo crontab -
+
+echo "Setup completed successfully!"
+
+
 
