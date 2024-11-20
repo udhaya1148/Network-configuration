7bef918cc8290d3e6174a2e3459dc30dcae674b4
udhaya1148 <59645398+udhaya1148@users.noreply.github.com>
2024-11-20 14:10:39 +0530

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -21,16 +21,16 @@ npx tailwindcss init -p
 # Network Configuration
 sudo cp -i /home/netcon/Network-configuration/PythonScript/Network-configuration.py /bin
 sudo chmod +x /bin/Network-configuration.py
-(crontab -l 2>/dev/null; echo "@reboot /usr/bin/python3 /bin/Network-configuration1.py") | sudo crontab -
+(crontab -l 2>/dev/null; echo "@reboot /usr/bin/python3 /bin/Network-configuration.py") | sudo crontab -
 
 # Static ARP
 sudo cp -i /home/netcon/Network-configuration/PythonScript/arp-pythonscript.py /bin
 sudo chmod +x /bin/arp-pythonscript.py
-(crontab -l 2>/dev/null; echo "@reboot /usr/bin/python3 /bin/arp-pythonscript1.py") | sudo crontab -
+(crontab -l 2>/dev/null; echo "@reboot /usr/bin/python3 /bin/arp-pythonscript.py") | sudo crontab -
 
 # React Application
 sudo chmod +x /home/netcon/Network-configuration/PythonScript/start-vite.sh
-(crontab -l 2>/dev/null; echo "@reboot /home/netcon/Network-configuration/PythonScript/start-vite1.sh") | sudo crontab -
+(crontab -l 2>/dev/null; echo "@reboot /home/netcon/Network-configuration/PythonScript/start-vite.sh") | sudo crontab -
 
 echo "Setup completed successfully!"
 
