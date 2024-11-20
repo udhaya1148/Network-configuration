37728ce2071f2b4a0e521ca91b0ce464040cbb2b
udhaya1148 <59645398+udhaya1148@users.noreply.github.com>
2024-11-20 11:48:00 +0530

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -5,5 +5,23 @@ sudo apt update
 # Install Gunicorn
 sudo apt install -y gunicorn
 
+# Install Dependencies  for flask
+sudo apt install python3-flask-cors
+sudo apt install python3-psutil
+
+# Install Dependencies for vite-react
+sudo apt install npm
+npm install react-router-dom
+npm install react-icons --save
+
+# Install TailwindCss
+npm install -D tailwindcss postcss autoprefixer
+npx tailwindcss init -p
+
+# add exection permission for file
+sudo chmod +x /bin/Network-configuration.py
+sudo chmod +x /bin/arp-pythonscript.py
+sudo chmod +x /home/netcon/Network-configuration/PythonScript/start-vite.sh
+
 
 
