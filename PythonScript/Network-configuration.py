967480f833c2623979d2e83ceb0a9ee65a8a4b1c
udhaya1148 <59645398+udhaya1148@users.noreply.github.com>
2024-11-22 11:21:51 +0530

    Update Network-configuration.py

--- PythonScript/Network-configuration.py
+++ PythonScript/Network-configuration.py
@@ -125,15 +125,8 @@ def update_network():
                 if not subnet or not ip or not gateway or not dns_servers:
                     return jsonify({'status': 'error', 'message': 'All fields are required when DHCP is disabled.'}), 400
 
-                #cidr_value = subnet_to_cidr(subnet) if '.' in subnet else subnet
-                if subnet.startswith('/'):
-                    cidr_value = subnet.split('/')[1]
-                elif subnet.count('.') == 3:
-                    cidr_value = subnet_to_cidr(subnet)
-                elif subnet.isdigit() and 0 <= int(subnet) <= 32:
-                    cidr_value = subnet
-                else:
-                    return jsonify({'status': 'error', 'message': 'Invalid subnet format.'}), 400
+                cidr_value = subnet_to_cidr(subnet) if '.' in subnet else subnet
+                
 
                 config['network']['ethernets'][interface]['dhcp4'] = False
                 config['network']['ethernets'][interface]['dhcp6'] = False
