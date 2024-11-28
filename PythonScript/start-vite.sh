10efcb07d4e8972eb6b1516620d7573cb6e9fb15
udhaya1148 <59645398+udhaya1148@users.noreply.github.com>
2024-11-28 11:08:55 +0530

    Create start-vite.sh

---
+++ PythonScript/start-vite.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Navigate to your project directory
+cd /root/Network-configuration/PythonScript
+
+# Build the project
+npm run build
+
+# Start the server using npm run preview
+npm run preview
